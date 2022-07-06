ee88e08fc0abac60fead26e35ccfaffdd3840ec1
Prem Kumar <prem_kumar@epam.com>
2022-07-06 18:58:47 -0400

    solution

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,7 +1,11 @@
 package batch
 
 import (
+	"context"
+	//"fmt"
 	"time"
+
+	"golang.org/x/sync/errgroup"
 )
 
 type user struct {
@@ -14,5 +18,20 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	g, _ := errgroup.WithContext(context.Background())
+	res = make([]user,n)
+    g.SetLimit(int(pool))
+	for i := 0; i < int(n); i++ {
+		i:=i
+		g.Go(func() error {
+				res[i] = getOne(int64(i))
+			    return nil
+		})
+	}
+	if err := g.Wait(); err != nil {
+		return nil
+	}
+	//fmt.Printf("res %v",res)
+	return res
+
 }
